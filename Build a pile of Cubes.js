fd5179d8630c4c0a544e2ef6dfb8eb7c05852980
Victoria Kovaliova <victkovaliova@gmail.com>
2022-09-14 10:17:43 +0300

    update

---
+++ Build a pile of Cubes.js
@@ -0,0 +1,14 @@
+function findNb(m) {
+    let n = 1
+    let sum = 0
+    while (sum < m) {
+        sum += n ** 3
+        n++
+    }
+    return sum > m ? -1 : n - 1
+}
+
+console.log(findNb(4183059834009));//2022
+console.log(findNb(24723578342962)); //-1
+console.log(findNb(135440716410000)); //4824
+console.log(findNb(40539911473216)); //3568
