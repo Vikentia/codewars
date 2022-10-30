d9d50e325c3b06f773cc012c81500ce4a7cf23ab
Victoria Kovaliova <victkovaliova@gmail.com>
2022-10-30 21:47:56 +0300

    update

---
+++ Find the stray number.js
@@ -0,0 +1,7 @@
+function stray(numbers) {
+    numbers.sort()
+    return numbers[0] === numbers[1] ? numbers[numbers.length - 1] : numbers[0];
+}
+
+console.log(stray([1, 1, 2]));
+console.log(stray([17, 17, 3, 17, 17, 17, 17]));
