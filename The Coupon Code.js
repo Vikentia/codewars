266e0e2b3054d60673ce52c15a75afbcb46f175d
Victoria Kovaliova <victkovaliova@gmail.com>
2023-03-28 08:42:07 +0300

    update

---
+++ The Coupon Code.js
@@ -0,0 +1,6 @@
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+    return enteredCode === correctCode && new Date(currentDate) < new Date(expirationDate)
+}
+
+console.log(checkCoupon('123a', '123', 'September 5, 2014', 'October 1, 2014'));
+console.log(checkCoupon('123', '123', 'September 5, 2014', 'October 1, 2014'));
