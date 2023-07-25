ef666f02f41916933864c76c062f8a6c9c9c79ad
Victoria Kovaliova <victkovaliova@gmail.com>
2023-07-25 17:18:51 +0200

    update

---
+++ Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language.js
@@ -0,0 +1,11 @@
+function isSameLanguage(list) {
+    let lang = list[0].language
+    return list.every(el => el.language === lang)
+}
+
+var list1 = [
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+console.log(isSameLanguage(list1));
